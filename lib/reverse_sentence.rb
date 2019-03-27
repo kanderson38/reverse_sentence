1311e59a2b4bb3fa8806ff651e08a3361bdfe28d
Kirsten Anderson <kirsten@kirstenmunson.com>
2019-03-27 10:35:46 -0700

    updated complexity explanation

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,7 +1,7 @@
 # A method to reverse the words in a sentence, in place.
 
 # Time complexity: O(n), where n = the length of the string. The program iterates over the string once in the reverse_words method and then over each word once again in the string_reverse method.
-# Space complexity: O(1). There are three variables that hold characters in the string_reverse method, and all changes to the string are made in place.
+# Space complexity: O(1). There is a variable that holds characters in the string_reverse method, and all changes to the string are made in place.
 
 def reverse_sentence(my_words)
   return nil if !my_words
